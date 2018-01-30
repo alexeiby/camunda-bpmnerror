d9a92f63f89706df39a3d215dff3ea21a9eb56eb
1 <1@Bolotin-12507.iba>
2018-01-30 23:47:50 +0300

    First upload

---
+++ src/main/java/by/iba/camunda/example/testprocess/TestProcessApplication.java
@@ -0,0 +1,10 @@
+package by.iba.camunda.example.testprocess;
+
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Test Process App")
+public class TestProcessApplication extends ServletProcessApplication {
+
+}
